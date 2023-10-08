583a1e04f0b6e151e23742d41b62ae36cba175b5
vorozhkog <vorozkhog@icloud.com>
2023-10-08 22:28:45 +0100

    conversion script done

--- src/convert.py
+++ src/convert.py
@@ -5,6 +5,8 @@ import src.settings as s
 from urllib.parse import unquote, urlparse
 from supervisely.io.fs import get_file_name, get_file_size
 import shutil
+import json
+from glob import glob
 
 from tqdm import tqdm
 
@@ -65,21 +67,81 @@ def count_files(path, extension):
             if file.endswith(extension):
                 count += 1
     return count
-    
+
+def create_ann(image_path):
+    filename = sly.fs.get_file_name_with_ext(image_path)
+    labels = []
+
+    height, width, tag_value = img_info_dict.get(filename)
+    if filename in ann_dict:
+        for bbox in ann_dict.get(filename):
+            class_id = bbox[1]
+            obj_class = classes_dict.get(int(class_id))
+            xc, yc, w, h = bbox[0]
+            xmin = xc - (w * 0.5)
+            ymin = yc - (h * 0.5)
+            xmax = xmin + w
+            ymax = ymin + h
+            rectangle = sly.Rectangle(ymin, xmin, ymax, xmax)
+            label = sly.Label(rectangle, obj_class)
+            labels.append(label)
+
+    img_tag = sly.Tag(tm_dc, tag_value)
+
+    return sly.Annotation(img_size=(height, width), labels=labels, img_tags=[img_tag])
+tm_dc = sly.TagMeta("date captured", value_type=sly.TagValueType.ANY_STRING)
+
+dataset_path = "/mnt/c/users/german/documents/hit-uav/normal_json"
+ann_path = dataset_path + "/annotations"
+
+id_filename_dict = {}
+img_info_dict = {}
+ann_id_dict = {}
+
+anns_paths = sly.fs.list_files(ann_path)
+for ann_file in anns_paths:
+    f = open(ann_file)
+    data = json.load(f)
+    for path in data["images"]:
+        id_filename_dict[path["id"]] = path["filename"]
+        img_info_dict[path["filename"]] = path["height"], path["width"], path["date_captured"]
+    for ann_path in data["annotation"]:
+        if ann_path["image_id"] not in ann_id_dict:
+            ann_id_dict[ann_path["image_id"]] = []
+        ann_id_dict[ann_path["image_id"]].append((ann_path["bbox"], ann_path["category_id"]))
+        
+ann_dict = {id_filename_dict.get(k): v for k, v in ann_id_dict.items()}
+
+classes_dict = {0: sly.ObjClass(name="person", geometry_type= sly.Rectangle,color=[0, 0, 255]),
+1: sly.ObjClass(name="car", geometry_type= sly.Rectangle,color=[255, 0, 255]),
+2: sly.ObjClass(name="bicycle", geometry_type= sly.Rectangle,color=[255, 0, 0]),
+3: sly.ObjClass(name="other vehicle", geometry_type= sly.Rectangle,color=[0, 255, 0]),
+4: sly.ObjClass(name="dontcare", geometry_type= sly.Rectangle,color=[0, 255, 255]),}
+
+
 def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
-    ### Function should read local dataset and upload it to Supervisely project, then return project info.###
-    raise NotImplementedError("The converter should be implemented manually.")
+    project = api.project.create(workspace_id, project_name)
+    meta = sly.ProjectMeta(obj_classes=list(classes_dict.values()), tag_metas=[tm_dc])
+    api.project.update_meta(project.id, meta.to_json())
 
-    # dataset_path = "/local/path/to/your/dataset" # general way
-    # dataset_path = download_dataset(teamfiles_dir) # for large datasets stored on instance
+    dir_paths = ["test", "train", "val"]
 
-    # ... some code here ...
-
-    # sly.logger.info('Deleting temporary app storage files...')
-    # shutil.rmtree(storage_dir)
-
-    # return project
+    batch_size = 50
+    for ds in dir_paths:
+        dspath = os.path.join(dataset_path, ds)
+        dataset = api.dataset.create(project.id, ds, change_name_if_conflict=True)
+        images_pathes = glob(os.path.join(dspath, "*"))
+        progress = sly.Progress("Create dataset {}".format(ds), len(images_pathes))
+        for img_pathes_batch in sly.batched(images_pathes, batch_size=batch_size):
+            img_names_batch = [
+                sly.fs.get_file_name_with_ext(im_path) for im_path in img_pathes_batch
+            ]
+            img_infos = api.image.upload_paths(dataset.id, img_names_batch, img_pathes_batch)
+            img_ids = [im_info.id for im_info in img_infos]
+            anns = [create_ann(image_path) for image_path in img_pathes_batch]
+            api.annotation.upload_anns(img_ids, anns)
+    return project
 
 
--- src/settings.py
+++ src/settings.py
@@ -13,37 +13,37 @@ from dataset_tools.templates import (
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = None
-PROJECT_NAME_FULL: str = None
+PROJECT_NAME: str = "HIT-UAV"
+PROJECT_NAME_FULL: str = "HIT-UAV: A high-altitude infrared thermal dataset for Unmanned Aerial Vehicle-based object detection"
 HIDE_DATASET = True  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
 ##################################
-LICENSE: License = None
-APPLICATIONS: List[Union[Industry, Domain, Research]] = None
-CATEGORY: Category = None
+LICENSE: License = License.CC0_1_0()
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Industry.SearchAndRescue(), Domain.DroneInspection()]
+CATEGORY: Category = Category.Aerial(extra=Category.Drones())
 
-CV_TASKS: List[CVTask] = None
-ANNOTATION_TYPES: List[AnnotationType] = None
+CV_TASKS: List[CVTask] = [CVTask.ObjectDetection()]
+ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.ObjectDetection()]
 
 RELEASE_DATE: Optional[str] = None  # e.g. "YYYY-MM-DD"
 if RELEASE_DATE is None:
-    RELEASE_YEAR: int = None
+    RELEASE_YEAR: int = 2023
 
-HOMEPAGE_URL: str = None
+HOMEPAGE_URL: str = "https://zenodo.org/record/7633134"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = None
+PREVIEW_IMAGE_ID: int = 6882390
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
-GITHUB_URL: str = None
+GITHUB_URL: str = "https://github.com/dataset-ninja/hit-uav"
 # URL to GitHub repo on dataset ninja (e.g. "https://github.com/dataset-ninja/some-dataset")
 
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = None
+DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = "https://github.com/suojiashun/HIT-UAV-Infrared-Thermal-Dataset/releases/download/v1.2.1/HIT-UAV.zip"
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
@@ -51,15 +51,15 @@ CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 
 # If you have more than the one paper, put the most relatable link as the first element of the list
 # Use dict key to specify name for a button
-PAPER: Optional[Union[str, List[str], Dict[str, str]]] = None
-BLOGPOST: Optional[Union[str, List[str], Dict[str, str]]] = None
-REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = None
+PAPER: Optional[Union[str, List[str], Dict[str, str]]] = "https://arxiv.org/abs/2204.03245"
+BLOGPOST: Optional[Union[str, List[str], Dict[str, str]]] = "https://www.nature.com/articles/s41597-023-02066-6"
+REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = "https://github.com/suojiashun/HIT-UAV-Infrared-Thermal-Dataset"
 
 CITATION_URL: Optional[str] = None
-AUTHORS: Optional[List[str]] = None
+AUTHORS: Optional[List[str]] = ["Suo, Jiashun", "Wang, Tianyi", "Zhang, Xingzhou", "Chen, Haiyang", "Zhou, Wei", "Shi, Weisong"]
 
-ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
-ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
+ORGANIZATION_NAME: Optional[Union[str, List[str]]] = "Pegasus Project, China"
+ORGANIZATION_URL: Optional[Union[str, List[str]]] = "https://pegasus.ac.cn/"
 
 # Set '__PRETEXT__' or '__POSTTEXT__' as a key with string value to add custom text. e.g. SLYTAGSPLIT = {'__POSTTEXT__':'some text}
 SLYTAGSPLIT: Optional[Dict[str, Union[List[str], str]]] = None
