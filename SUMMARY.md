989435b17996a5a4771c505ed6e71b6c2af71fb9
Ars Vladimirov <grokhael@gmail.com>
2023-11-07 00:54:09 +0600

    fix convert

--- SUMMARY.md
+++ SUMMARY.md
@@ -2,6 +2,6 @@
 
 The dataset consists of 2898 images with 24899 labeled objects belonging to 5 different classes including *person*, *car*, *bicycle*, and other: *other vehicle* and *dontcare*.
 
-Images in the HIT-UAV dataset have bounding box annotations. There are 32 (1% of the total) unlabeled images (i.e. without annotations). There are 3 splits in the dataset: *test* (579 images), *train* (2029 images), and *val* (290 images). The dataset was released in 2023 by the Pegasus Project, China.
+Images in the HIT-UAV dataset have bounding box annotations. There are 32 (1% of the total) unlabeled images (i.e. without annotations). There are 3 splits in the dataset: *train* (2029 images), *test* (579 images), and *val* (290 images). The dataset was released in 2023 by the Pegasus Project, China.
 
 <img src="https://github.com/dataset-ninja/hit-uav/raw/main/visualizations/poster.png">
--- src/convert.py
+++ src/convert.py
@@ -1,14 +1,15 @@
-import supervisely as sly
-import os
-from dataset_tools.convert import unpack_if_archive
-import src.settings as s
-from urllib.parse import unquote, urlparse
-from supervisely.io.fs import get_file_name, get_file_size
-import shutil
 import json
+import os
+import shutil
 from glob import glob
+from urllib.parse import unquote, urlparse
 
+import supervisely as sly
+from dataset_tools.convert import unpack_if_archive
+from supervisely.io.fs import get_file_name, get_file_size
 from tqdm import tqdm
+
+import src.settings as s
 
 
 def download_dataset(teamfiles_dir: str) -> str:
@@ -71,81 +72,79 @@ def count_files(path, extension):
     return count
 
 
-def create_ann(image_path):
-    filename = sly.fs.get_file_name_with_ext(image_path)
-    labels = []
-
-    height, width, date_captured = img_info_dict.get(filename)
-    if filename in ann_dict:
-        for bbox in ann_dict.get(filename):
-            class_id = bbox[1]
-            obj_class = classes_dict.get(int(class_id))
-            xc, yc, w, h = bbox[0]
-            x1 = xc
-            y1 = yc
-            x2 = x1 + w
-            y2 = y1 + h
-            rectangle = sly.Rectangle(top=y1, left=x1, bottom=y2, right=x2)
-            label = sly.Label(rectangle, obj_class)
-            labels.append(label)
-
-    filename_split = filename.split("_")
-    img_tag_dc = sly.Tag(
-        tm_dc,
-        value=(date_captured[0:4] + "/" + date_captured[5:6] + "/" + date_captured[7:8]),
-    )
-    img_tag_st = shooting_time.get(int(filename_split[0]))
-    img_tag_fa = sly.Tag(tm_fa, int(filename_split[1]))
-    img_tag_cp = sly.Tag(tm_cp, int(filename_split[2]))
-
-    return sly.Annotation(
-        img_size=(height, width),
-        labels=labels,
-        img_tags=[img_tag_dc, img_tag_st, img_tag_fa, img_tag_cp],
-    )
-
-
-dataset_path = "/mnt/c/users/german/documents/hit-uav/normal_json"
-ann_path = dataset_path + "/annotations"
-
-id_filename_dict = {}
-img_info_dict = {}
-ann_id_dict = {}
-
-anns_paths = sly.fs.list_files(ann_path)
-for ann_file in anns_paths:
-    f = open(ann_file)
-    data = json.load(f)
-    for path in data["images"]:
-        id_filename_dict[path["id"]] = path["filename"]
-        img_info_dict[path["filename"]] = path["height"], path["width"], path["date_captured"]
-    for ann_path in data["annotation"]:
-        if ann_path["image_id"] not in ann_id_dict:
-            ann_id_dict[ann_path["image_id"]] = []
-        ann_id_dict[ann_path["image_id"]].append((ann_path["bbox"], ann_path["category_id"]))
-
-ann_dict = {id_filename_dict.get(k): v for k, v in ann_id_dict.items()}
-
-tm_dc = sly.TagMeta("date captured", value_type=sly.TagValueType.ANY_STRING)
-tm_day = sly.TagMeta("day", sly.TagValueType.NONE)
-tm_night = sly.TagMeta("night", sly.TagValueType.NONE)
-tm_fa = sly.TagMeta("flight altitude", sly.TagValueType.ANY_NUMBER)
-tm_cp = sly.TagMeta("camera perspective", sly.TagValueType.ANY_NUMBER)
-
-shooting_time = {0: tm_day, 1: tm_night}
-
-classes_dict = {
-    0: sly.ObjClass(name="person", geometry_type=sly.Rectangle, color=[0, 0, 255]),
-    1: sly.ObjClass(name="car", geometry_type=sly.Rectangle, color=[255, 0, 255]),
-    2: sly.ObjClass(name="bicycle", geometry_type=sly.Rectangle, color=[255, 0, 0]),
-    3: sly.ObjClass(name="other vehicle", geometry_type=sly.Rectangle, color=[0, 255, 0]),
-    4: sly.ObjClass(name="dontcare", geometry_type=sly.Rectangle, color=[0, 255, 255]),
-}
-
-
 def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
+    dataset_path = "/mnt/c/users/german/documents/hit-uav/normal_json"
+    ann_path = dataset_path + "/annotations"
+
+    def create_ann(image_path):
+        filename = sly.fs.get_file_name_with_ext(image_path)
+        labels = []
+
+        height, width, date_captured = img_info_dict.get(filename)
+        if filename in ann_dict:
+            for bbox in ann_dict.get(filename):
+                class_id = bbox[1]
+                obj_class = classes_dict.get(int(class_id))
+                xc, yc, w, h = bbox[0]
+                x1 = xc
+                y1 = yc
+                x2 = x1 + w
+                y2 = y1 + h
+                rectangle = sly.Rectangle(top=y1, left=x1, bottom=y2, right=x2)
+                label = sly.Label(rectangle, obj_class)
+                labels.append(label)
+
+        filename_split = filename.split("_")
+        img_tag_dc = sly.Tag(
+            tm_dc,
+            value=(date_captured[0:4] + "/" + date_captured[5:6] + "/" + date_captured[7:8]),
+        )
+        img_tag_st = shooting_time.get(int(filename_split[0]))
+        img_tag_fa = sly.Tag(tm_fa, int(filename_split[1]))
+        img_tag_cp = sly.Tag(tm_cp, int(filename_split[2]))
+
+        return sly.Annotation(
+            img_size=(height, width),
+            labels=labels,
+            img_tags=[img_tag_dc, img_tag_st, img_tag_fa, img_tag_cp],
+        )
+
+    id_filename_dict = {}
+    img_info_dict = {}
+    ann_id_dict = {}
+
+    anns_paths = sly.fs.list_files(ann_path)
+    for ann_file in anns_paths:
+        f = open(ann_file)
+        data = json.load(f)
+        for path in data["images"]:
+            id_filename_dict[path["id"]] = path["filename"]
+            img_info_dict[path["filename"]] = path["height"], path["width"], path["date_captured"]
+        for ann_path in data["annotation"]:
+            if ann_path["image_id"] not in ann_id_dict:
+                ann_id_dict[ann_path["image_id"]] = []
+            ann_id_dict[ann_path["image_id"]].append((ann_path["bbox"], ann_path["category_id"]))
+
+    ann_dict = {id_filename_dict.get(k): v for k, v in ann_id_dict.items()}
+
+    tm_dc = sly.TagMeta("date captured", value_type=sly.TagValueType.ANY_STRING)
+    tm_day = sly.TagMeta("day", sly.TagValueType.NONE)
+    tm_night = sly.TagMeta("night", sly.TagValueType.NONE)
+    tm_fa = sly.TagMeta("flight altitude", sly.TagValueType.ANY_NUMBER)
+    tm_cp = sly.TagMeta("camera perspective", sly.TagValueType.ANY_NUMBER)
+
+    shooting_time = {0: tm_day, 1: tm_night}
+
+    classes_dict = {
+        0: sly.ObjClass(name="person", geometry_type=sly.Rectangle, color=[0, 0, 255]),
+        1: sly.ObjClass(name="car", geometry_type=sly.Rectangle, color=[255, 0, 255]),
+        2: sly.ObjClass(name="bicycle", geometry_type=sly.Rectangle, color=[255, 0, 0]),
+        3: sly.ObjClass(name="other vehicle", geometry_type=sly.Rectangle, color=[0, 255, 0]),
+        4: sly.ObjClass(name="dontcare", geometry_type=sly.Rectangle, color=[0, 255, 255]),
+    }
+
     project = api.project.create(workspace_id, project_name)
     meta = sly.ProjectMeta(
         obj_classes=list(classes_dict.values()), tag_metas=[tm_dc, tm_day, tm_night, tm_fa, tm_cp]
